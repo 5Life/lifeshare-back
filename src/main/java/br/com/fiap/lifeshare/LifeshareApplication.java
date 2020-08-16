50c99baa224fc4c86fa69b3ab2674edb7af259fc
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-08-16 20:19:45 -0300

    creating project

---
+++ src/main/java/br/com/fiap/lifeshare/LifeshareApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.lifeshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeshareApplication.class, args);
+	}
+
+}
