346654386958e0bc7afa87631caffd37a656eea8
Rafael Leão <rleao53@gmail.com>
2020-08-19 00:13:19 -0300

    Adjust attribute from name to email

--- src/main/java/br/com/fiap/lifeshare/config/auth/UserAuthDTO.java
+++ src/main/java/br/com/fiap/lifeshare/config/auth/UserAuthDTO.java
@@ -3,18 +3,18 @@ package br.com.fiap.lifeshare.config.auth;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 public class UserAuthDTO {
-    private String nome;
+    private String email;
     private String senha;
 
     public String getSenha() {
         return senha;
     }
 
-    public String getNome() {
-        return nome;
+    public String getEmail() {
+        return email;
     }
 
     public UsernamePasswordAuthenticationToken convert() {
-        return new UsernamePasswordAuthenticationToken(this.nome, this.senha);
+        return new UsernamePasswordAuthenticationToken(this.email, this.senha);
     }
 }
