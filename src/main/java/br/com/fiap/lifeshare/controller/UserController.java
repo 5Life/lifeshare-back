a43ffa66ce464384a22d097962553fbcdcccd333
Rafael Leão <rleao53@gmail.com>
2020-08-20 21:43:24 -0300

    Add service layer

--- src/main/java/br/com/fiap/lifeshare/controller/UserController.java
+++ src/main/java/br/com/fiap/lifeshare/controller/UserController.java
@@ -3,10 +3,10 @@ package br.com.fiap.lifeshare.controller;
 import br.com.fiap.lifeshare.dto.UserDTO;
 import br.com.fiap.lifeshare.model.User;
 import br.com.fiap.lifeshare.repository.UserRepository;
+import br.com.fiap.lifeshare.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,28 +18,19 @@ import java.util.List;
 @RequestMapping("/api")
 public class UserController {
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody UserDTO userDTO) {
         try {
-            User user = userDTO.convert();
-            user.setSenha(new BCryptPasswordEncoder().encode(user.getSenha()));
-            userRepository.save(user);
-            return new ResponseEntity<>("Usuario criado", HttpStatus.OK);
+            return new ResponseEntity<>(userService.create(userDTO), HttpStatus.CREATED);
         } catch (Exception e) {
-            e.printStackTrace();
             return new ResponseEntity<>("Usuario não pode ser criado", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping
     public ResponseEntity<List<UserDTO>> read(){
-        List<User> users = userRepository.findAll();
-        List<UserDTO> userDTOS = new ArrayList<>();
-        for (User user: users) {
-            userDTOS.add(user.convert());
-        }
-        return new ResponseEntity<List<UserDTO>>(userDTOS, HttpStatus.OK);
+        return new ResponseEntity<>(userService.read(), HttpStatus.OK);
     }
 }
