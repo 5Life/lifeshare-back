7a24ece3c907091cca2c70938f280f2e0bc028f3
Gabriel Vacari <gabriel_vacari@outlook.com>
2020-08-29 23:53:29 -0300

    create test for user

---
+++ src/test/java/br/com/fiap/lifeshare/infrasctructure/http/routing/UserControllerTest.java
@@ -0,0 +1,39 @@
+package br.com.fiap.lifeshare.infrasctructure.http.routing;
+
+import br.com.fiap.lifeshare.infrasctructure.http.presentation.request.UserDTOFixture;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+    @Test
+    void shouldCreateNewUser() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(UserDTOFixture.getNewUser())
+        ).andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    void shouldntCreateNewUser() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(UserDTOFixture.getUserWithoutPassword())
+        ).andExpect(MockMvcResultMatchers.status().isInternalServerError());
+    }
+
+}
