9776c4c8f5ef997e556131e7e70b3f6e6ea8b45b
Rafael Leão <rleao53@gmail.com>
2020-10-25 13:52:10 -0300

    Add CrossOrigin anotation

--- src/main/java/br/com/fiap/lifeshare/controller/UserController.java
+++ src/main/java/br/com/fiap/lifeshare/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,6 +23,7 @@ import br.com.fiap.lifeshare.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
